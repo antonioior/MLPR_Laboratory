17614b43af38b32bff051c7427da259ccbad82c6
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-03-21 11:18:52 +0100

    completed first part of lab 1

---
+++ 01. Lab 01/01. Exercise 01/competitor.py
@@ -0,0 +1,16 @@
+class Competitor:
+    def __init__(self, name, surname, nationality,scores):
+        self.name = name
+        self.surname = surname
+        self.nationality = nationality
+        self.scores = scores
+        self.final_score = 0
+
+    def calculate_final_score(self):
+        self.final_score = sum([score for score in self.scores])
+        min_score = min([score for score in self.scores])
+        max_score = max([score for score in self.scores])
+        self.final_score = self.final_score - min_score - max_score
+
+    def __str__(self):
+        return f"{self.name} {self.surname} {self.nationality} {self.final_score:.2f}"
--- 01. Lab 01/01. Exercise 01/main.py
+++ 01. Lab 01/01. Exercise 01/main.py
@@ -1,4 +1,23 @@
+import sys 
+from competitor import *
+
+def load(nameFile):
+    rows = open(nameFile, 'r')
+    competitors = []
+    for row in rows:
+        row = row.strip().split(' ')
+        name = row[0]
+        surname = row[1]
+        nationality = row[2]
+        scores = [float(score) for score in row[3:]]
+        competitor = Competitor(name, surname, nationality, scores)
+        competitors.append(competitor)
+    return competitors
+
 if __name__ == "__main__":
-    a = 5
-    b = a + 10
-    print("a = ", a)
+    print("Name file is", sys.argv[1])
+    competitors = load(sys.argv[1])
+    [competitor.calculate_final_score() for competitor in competitors]
+    best_competitors = sorted(competitors, key=lambda x: x.final_score, reverse=True)[:3]
+    print("Final ranking:\n")
+    [print(competitor) for competitor in best_competitors]
