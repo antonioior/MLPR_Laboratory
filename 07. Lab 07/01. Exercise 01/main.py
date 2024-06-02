77012564c9f8f726ef013e70bf031977477007b8
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-06-02 16:57:28 +0200

    Feat: Lab 07: Binary task: Evaluation and minimum detection costs

--- 07. Lab 07/01. Exercise 01/main.py
+++ 07. Lab 07/01. Exercise 01/main.py
@@ -2,7 +2,7 @@ from Commedia import computeConfusionMatrixCommedia, LoadllrLTEInfPar, computePV
 from Iris import computeConfusionMatrixIris
 from load import load
 from printValue import printConfusionMatrix
-from utils import split_db_2to1, computeConfusionMatrix
+from utils import split_db_2to1, computeConfusionMatrix, computeDCF, computeDCFNormalized, compute_minDCF_binary_fast
 
 if __name__ == "__main__":
     printData = True
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     confusionMatrix_Commedia = computeConfusionMatrixCommedia()
 
-    # Optimal Decision
+    # OPTIMAL DECISIONS
     llrInf_Par, LTE_Inf_Par = LoadllrLTEInfPar()
     valueConfiguration = {
         "one": [0.5, 1, 1],
@@ -34,8 +34,31 @@ if __name__ == "__main__":
                                            valueConfiguration[key][2])
         valueConfusionMatrix[key] = computeConfusionMatrix(PVAL_Inf_Par, LTE_Inf_Par)
 
+    # EVALUATION
+    dcf = {}
+    dcfNormalized = {}
+    for key in valueConfiguration:
+        dcf[key] = computeDCF(
+            valueConfusionMatrix[key],
+            valueConfiguration[key][0],
+            valueConfiguration[key][1],
+            valueConfiguration[key][2])
+        dcfNormalized[key] = computeDCFNormalized(
+            valueConfusionMatrix[key],
+            valueConfiguration[key][0],
+            valueConfiguration[key][1],
+            valueConfiguration[key][2])
+
+    # minimum detection costs
+    minDCF = {}
+    for key in valueConfiguration:
+        minDCF[key] = compute_minDCF_binary_fast(llrInf_Par, LTE_Inf_Par, valueConfiguration[key][0],
+                                                 valueConfiguration[key][1],
+                                                 valueConfiguration[key][2])
+
     if printData:
         print("MAIN - RESULT")
+        # CONFUSION MATRIX
         print("\tCONFUSION MATRIX")
         print("\tConfusion matrix for MVG:")
         printConfusionMatrix(confusionMatrix_MVG)
@@ -44,8 +67,26 @@ if __name__ == "__main__":
         print("\tConfusion matrix for Commedia:")
         printConfusionMatrix(confusionMatrix_Commedia)
 
+        # OPTIMAL DECISIONS
         print("\tOPTIMAL DECISIONS")
         for key in valueConfusionMatrix:
             print(f"\tConfusion matrix for Inf_Par with configuration:\n"
                   f"\tpi = {valueConfiguration[key][0]}, Cfn = {valueConfiguration[key][1]}, Cfp = {valueConfiguration[key][2]}")
             printConfusionMatrix(valueConfusionMatrix[key])
+
+        # EVALUATION
+        print("\tEVALUATION")
+        print("\t\t(pi, Cfn, Cfp) -> DCF")
+        for key in dcf:
+            print(
+                f"\t\t{valueConfiguration[key][0], valueConfiguration[key][1], valueConfiguration[key][2]} -> {dcf[key]: 3.3f}")
+
+        print("\n\t\t(pi, Cfn, Cfp) -> DCFn")
+        for key in dcfNormalized:
+            print(
+                f"\t\t{valueConfiguration[key][0], valueConfiguration[key][1], valueConfiguration[key][2]} -> {dcfNormalized[key]: 3.3f}")
+
+        print("\n\t\t(pi, Cfn, Cfp) -> min DCF")
+        for key in minDCF:
+            print(
+                f"\t\t{valueConfiguration[key][0], valueConfiguration[key][1], valueConfiguration[key][2]} -> {minDCF[key]: 3.3f}")
