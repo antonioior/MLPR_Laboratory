95caf569af4aa7cf3dbf734d7adc4b5d35273652
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-06-08 12:39:13 +0200

    Feat: Lab 08: Numerical optimization done

---
+++ 08. Lab 08/01. Exercise 01/function.py
@@ -0,0 +1,35 @@
+import numpy as np
+import scipy.optimize as sp
+
+
+def f(x):
+    y, z = x
+    return (y + 3) ** 2 + np.sin(y) + (z + 1) ** 2
+
+
+def fWithGradient(x):
+    y, z = x
+    f = (y + 3) ** 2 + np.sin(y) + (z + 1) ** 2
+    gradientY = 2 * (y + 3) + np.cos(y)
+    gradientZ = 2 * (z + 1)
+    return f, np.array([gradientY, gradientZ])
+
+
+def computeMinFWithApproxGrad(inputFun, x0, iprint, printResult=False):
+    x, f, d = sp.fmin_l_bfgs_b(func=inputFun, x0=x0, approx_grad=True, iprint=iprint)
+
+    if printResult:
+        print("Result with approximated gradient:")
+        print(f"\tMimimum is in {x}")
+        print(f"\tValue to minimum point is {f: .9f}")
+        print(f"\tNumer of time that f was called {d["funcalls"]}")
+
+
+def computeMinFWithoutApproxGrad(inputFun, x0, iprint, printResult=False):
+    x, f, d = sp.fmin_l_bfgs_b(func=inputFun, x0=x0, iprint=iprint)
+
+    if printResult:
+        print("Result without approximated gradient:")
+        print(f"\tMimimum is in {x}")
+        print(f"\tValue to minimum point is {f: .9f}")
+        print(f"\tNumer of time that f was called {d["funcalls"]}")
--- 08. Lab 08/01. Exercise 01/main.py
+++ 08. Lab 08/01. Exercise 01/main.py
@@ -1,2 +1,8 @@
+import numpy as np
+
+from function import f, fWithGradient, computeMinFWithApproxGrad, computeMinFWithoutApproxGrad
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    printResult = True
+    computeMinFWithApproxGrad(inputFun=f, x0=np.array([0, 0]), iprint=-1, printResult=printResult)
+    computeMinFWithoutApproxGrad(inputFun=fWithGradient, x0=np.array([0, 0]), iprint=-1, printResult=printResult)
