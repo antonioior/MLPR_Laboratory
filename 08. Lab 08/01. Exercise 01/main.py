Fix: Lab 08: Numerical optimization wrapped in a function

08. Lab 08/01. Exercise 01/main.py
@@ -1,8 +1,6 @@
-import numpy as np
-
-from function import f, fWithGradient, computeMinFWithApproxGrad, computeMinFWithoutApproxGrad
+from function import numericalOptimization
 
 if __name__ == "__main__":
     printResult = True
-    computeMinFWithApproxGrad(inputFun=f, x0=np.array([0, 0]), iprint=-1, printResult=printResult)
-    computeMinFWithoutApproxGrad(inputFun=fWithGradient, x0=np.array([0, 0]), iprint=-1, printResult=printResult)
+    # Numerical Optimization
+    numericalOptimization(printResult=printResult)