1196af3be039d129d14d0ad241741130588de3ea
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-06-15 16:10:36 +0200

    Fix: Lab 09: Laboratory part completed

--- 09. Lab 09/01. Exercise 01/main.py
+++ 09. Lab 09/01. Exercise 01/main.py
@@ -7,6 +7,6 @@ from utils import split_db_2to1
 if __name__ == "__main__":
     D, L = load_iris_binary()
     (DTR, LTR), (DVAL, LVAL) = split_db_2to1(D, L)
-    linearSVM(DTR, LTR, DVAL, LVAL, printResult=True)
-    polinomialSVM(DTR, LTR, DVAL, LVAL, printResult=True)
-    radialSVM(DTR, LTR, DVAL, LVAL, printResult=True)
+    linearSVM(DTR, LTR, DVAL, LVAL, printResult=False)
+    polinomialSVM(DTR, LTR, DVAL, LVAL, printResult=False)
+    radialSVM(DTR, LTR, DVAL, LVAL, printResult=False)
