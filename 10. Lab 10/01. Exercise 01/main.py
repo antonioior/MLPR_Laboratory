136648aaa8e3db2b4a5146506dadcd46a97b86dd
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-06-21 18:37:42 +0200

    Feat: Lab 10: GMM done

--- 10. Lab 10/01. Exercise 01/main.py
+++ 10. Lab 10/01. Exercise 01/main.py
@@ -1,2 +1,30 @@
+import numpy as np
+
+from load import load_gmm
+from utils import logpdf_GMM
+
 if __name__ == "__main__":
-    pass
+    printResult = True
+
+    # WITH 4D DATA
+    data = np.load("../Data/GMM_data_4D.npy")
+    gmm = load_gmm("../Data/GMM_4D_3G_init.json")
+    logdensResult4D = np.load("../Data/GMM_4D_3G_init_ll.npy")
+    logdens4D = logpdf_GMM(data, gmm)
+
+    # WITH 1D DATA
+    data = np.load("../Data/GMM_data_1D.npy")
+    gmm = load_gmm("../Data/GMM_1D_3G_init.json")
+    logdensResult1D = np.load("../Data/GMM_1D_3G_init_ll.npy")
+    logdens1D = logpdf_GMM(data, gmm)
+
+    if printResult:
+        print("RESULT GMM")
+        print("\t Logdens result prof 4D")
+        print(logdensResult4D)
+        print("\t Logdens calculated 4D")
+        print(logdens4D)
+        print("\t Logdens result prof 1D")
+        print(logdensResult1D)
+        print("\t Logdens calculated 1D")
+        print(logdens1D)
---
+++ 10. Lab 10/01. Exercise 01/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+import scipy as sp
+
+
+# LAB 5
+def logpdf_GAU_ND(x, mu, C):
+    P = np.linalg.inv(C)
+    return -0.5 * x.shape[0] * np.log(np.pi * 2) - 0.5 * np.linalg.slogdet(C)[1] - 0.5 * (
+            (x - mu) * (P @ (x - mu))).sum(0)
+
+
+# LAB 10
+def logpdf_GMM(X, gmm):
+    M = len(gmm)
+    N = X.shape[1]
+    S = np.zeros((M, N))
+    for g in range(M):
+        w, mu, C = gmm[g]
+        S[g, :] = logpdf_GAU_ND(X, mu, C) + np.log(w)
+    logdens = sp.special.logsumexp(S, axis=0)
+    return logdens
