b16e53164387723be5a91b2160dc968039158f42
ANTONIO IORIO s317748 <s317748@studenti.polito.it>
2024-06-28 10:01:49 +0200

    Feat: Lab 10: Init project part

---
+++ Project/Code/10. Lab 10/PolinomialSVM.py
@@ -0,0 +1,84 @@
+import numpy as np
+import scipy.optimize as sp
+
+from DCF import minDCF, actDCF
+from SVMClass import SVM
+from graph import createMinDCFActDCFPlot
+
+
+def polinomialSVM(DTR, LTR, DVAL, LVAL, printResult=True, titleGraph=""):
+    resultPolynomial = {}
+    count = 0
+    CValues = np.logspace(-5, 0, 11)
+    zeta = 0
+    K = 0
+    c = 1
+    d = 2
+    priorT = 0.1
+    for C in CValues:
+        resultPolynomial["config" + str(count)] = {
+            "K": K,
+            "C": C,
+            "c": c
+        }
+        polinomial = SVM(DTR, LTR, C, K, "polinomial", c=c, d=d)
+        alphaStar, _, _ = sp.fmin_l_bfgs_b(func=polinomial.fOpt,
+                                           x0=np.zeros(polinomial.getDTRExtend().shape[1]),
+                                           approx_grad=False,
+                                           maxfun=15000,
+                                           factr=1.0,
+                                           bounds=[(0, C) for i in LTR],
+                                           )
+
+        resultPolynomial["config" + str(count)]["dualLoss"] = -polinomial.fOpt(alphaStar)[0][0]
+
+        sllr = polinomial.computeScore(
+            alphaStar=alphaStar,
+            D2=DVAL)
+        Pval = (sllr > 0) * 1
+        resultPolynomial["config" + str(count)]["errorRate"] = (Pval != LVAL).sum() / float(LVAL.size)
+        resultPolynomial["config" + str(count)]["minDCF"] = minDCF(sllr, LVAL, priorT, 1, 1)
+        resultPolynomial["config" + str(count)]["actDCF"] = actDCF(sllr, LVAL, priorT, 1, 1)
+
+        count += 1
+
+    if printResult:
+        x = []
+        yMinDCF = []
+        yActDCF = []
+        print("RESULT POLYNOMIAL SVM")
+        for key in resultPolynomial.keys():
+            print(
+                f"\t{key} where K={resultPolynomial[key]["K"]} and C = {resultPolynomial[key]["C"]}, Poly(d = {d}, c = {resultPolynomial[key]["c"]})")
+            print(f"\t\tDual Loss: {resultPolynomial[key]["dualLoss"]:.6e}")
+            print(f"\t\tError Rate: {resultPolynomial[key]["errorRate"] * 100:.1f}%")
+            print(f"\t\tminDCF: {resultPolynomial[key]["minDCF"]:.4f}")
+            print(f"\t\tactDCF: {resultPolynomial[key]["actDCF"]:.4f}")
+            print()
+            x.append(resultPolynomial[key]["C"])
+            yMinDCF.append(resultPolynomial[key]["minDCF"])
+            yActDCF.append(resultPolynomial[key]["actDCF"])
+
+        createMinDCFActDCFPlot(
+            x=x,
+            yMinDCF=yMinDCF,
+            yActDCF=yActDCF,
+            xLabel="C",
+            colorMinDCF="blue",
+            colorActDCF="red",
+            title=titleGraph,
+            togheter=True,
+            logScale=True
+        )
+
+        createMinDCFActDCFPlot(
+            x=x,
+            yMinDCF=yMinDCF,
+            yActDCF=yActDCF,
+            xLabel="C",
+            colorMinDCF="blue",
+            colorActDCF="red",
+            title=titleGraph,
+            togheter=False,
+            logScale=True
+        )
---
+++ Project/Code/10. Lab 10/QuadraticLogisticRegression.py
@@ -0,0 +1,103 @@
+import numpy as np
+import scipy.optimize as sp
+
+from DCF import minDCF, actDCF
+from graph import createMinDCFActDCFPlot
+from utils import vcol, vrow, errorRate
+
+
+def QuadraticLogisticRegression(DTR, LTR, DVAL, LVAL, titleGraph, printResult=False):
+    lamb = np.logspace(-4, 2, 13)
+    result = {}
+
+    for i in range(len(lamb)):
+        result["lamb" + str(i)] = {"lambda": lamb[i]}
+
+    for i in range(len(lamb)):
+        logRegObj = quadraticLogClass(DTR, LTR, result["lamb" + str(i)]["lambda"])
+        vf = sp.fmin_l_bfgs_b(func=logRegObj.logreg_obj, x0=np.zeros(logRegObj.DTR.shape[0] + 1),
+                              approx_grad=False, maxfun=15000)[0]
+        result["lamb" + str(i)]["J"] = logRegObj.logreg_obj(vf)[0]
+        DVAL_expanded = expandeFeature(DVAL)
+        result["lamb" + str(i)]["ErrorRate"], sVal = errorRate(DVAL_expanded, LVAL, vf)
+
+        pEmp = (LTR == 1).sum() / LTR.size
+        sllr = sVal - np.log(pEmp / (1 - pEmp))
+        result["lamb" + str(i)]["minDCF"] = minDCF(sllr, LVAL, 0.1, 1, 1)
+        result["lamb" + str(i)]["actDCF"] = actDCF(sllr, LVAL, 0.1, 1, 1)
+
+    if printResult:
+        x = []
+        yMinDCF = []
+        yActDCF = []
+        print("RESULT FOR BINARY LOGISTIC REGRESSION")
+        for lamb in result.keys():
+            print(f"\tLambda: {lamb}")
+            print(f"\t\tJ: {result[lamb]["J"]: .6e}")
+            print(f"\t\tError: {result[lamb]["ErrorRate"] * 100:.1f} %")
+            print(f"\t\tminDCF: {result[lamb]['minDCF']:.4f}")
+            print(f"\t\tactDCF: {result[lamb]['actDCF']:.4f}")
+            print()
+            x.append(result[lamb]["lambda"])
+            yMinDCF.append(result[lamb]["minDCF"])
+            yActDCF.append(result[lamb]["actDCF"])
+
+        createMinDCFActDCFPlot(
+            x=x,
+            yMinDCF=yMinDCF,
+            yActDCF=yActDCF,
+            colorMinDCF="blue",
+            colorActDCF="red",
+            title=titleGraph,
+            togheter=True,
+            logScale=True
+        )
+
+        createMinDCFActDCFPlot(
+            x=x,
+            yMinDCF=yMinDCF,
+            yActDCF=yActDCF,
+            colorMinDCF="blue",
+            colorActDCF="red",
+            title=titleGraph,
+            togheter=False,
+            logScale=True
+        )
+
+
+class quadraticLogClass:
+
+    def __init__(self, DTR, LTR, l):
+        self.DTR = expandeFeature(DTR)
+        self.LTR = LTR
+        self.l = l
+
+    def logreg_obj(self, v):
+        w, b = v[:-1], v[-1]
+        S = np.dot(vcol(w).T, self.DTR).ravel() + b
+
+        regressionTerm = self.l / 2 * np.linalg.norm(w) ** 2
+
+        ZTR = 2. * self.LTR - 1.
+        loss = np.logaddexp(0, -S * ZTR)
+        J = regressionTerm + loss.mean()
+
+        G = -ZTR / (1 + np.exp(ZTR * S))
+
+        gradB = G.mean()
+        gradW = self.l * w.ravel() + (vrow(G) * self.DTR).mean(1)
+        grad = np.hstack([gradW, np.array(gradB)])
+        return J, grad
+
+
+def expandeFeature(D):
+    D_exp = np.zeros(shape=(D.shape[0] * D.shape[0] + D.shape[0], D.shape[1]))
+    for i in range(D.shape[1]):
+        xi = D[:, i:i + 1]
+        D_exp[:, i:i + 1] = np.vstack((vectorize(np.dot(xi, xi.T)), xi))
+    return D_exp
+
+
+def vectorize(M):
+    M_vec = np.hstack(M).reshape(-1, 1)
+    return M_vec
