e6cde58f3dbee4561255c29d1fababad381d2b21
antonioior <antonio_ior@live.com>
2024-08-27 19:08:23 +0200

    Fix: Calibration SVG Feat: Added calibragion GMM Feat: Completed report part on Lab 05

--- Project/Code/11. Lab 11/Calibration.py
+++ Project/Code/11. Lab 11/Calibration.py
@@ -4,8 +4,9 @@ import scipy.optimize as sp
 from QuadraticLogisticRegression import quadraticLogClass
 from DCF import bayesError, minDCF, actDCF
 from graph import createBayesErrorPlots
-from utils import vrow
+from utils import vrow, logpdf_GMM
 from SVMClass import SVM
+from LBG import trainGMMReturnMinAndActDCF, trainGMMCalibrationReturnMinAndActDCF
 
 
 def calibration(DTR, LTR, DVAL, LVAL, printResult=False):
@@ -15,24 +16,22 @@ def calibration(DTR, LTR, DVAL, LVAL, printResult=False):
     for priorCal in priorCals:
         calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
         calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
+        calibrationGMM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
 
 
+# CALIBRATION LOGISTIC REGRESSION
 def calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult):
     l = 3.162278e-2
-
     logRegQDT = quadraticLogClass(DTR, LTR, l)
     sllrWithoutCal, minDCFWithoutCal, actDCFWithoutCal, score = logRegQDT.trainReturnMinAndActDCF(DVAL, LVAL, priorT)
-
     minDCFKFold, actDCFKFold, llrK, labelK = logRegQDT.trainCalibrationReturnMinAndActDCF(K, priorCal, priorT, score,
                                                                                           sllrWithoutCal, LVAL)
-
     if printResult:
         print(f"priorCal {priorCal}")
         print("\tCALIBRATION QUADRATIC LOGISTIC REGRESSION")
         print(f"\tLambda: {l}")
         print(f"\t\tminDCF: {minDCFWithoutCal:.4f}")
         print(f"\t\tactDCF: {actDCFWithoutCal:.4f}")
-
         print(f"\t\tminDCF - cal: {minDCFKFold:.4f}")
         print(f"\t\tactDCF - cal: {actDCFKFold:.4f}")
         effPriorLogOdds, actDCFWithoutCalBayesError, minDCFWithoutCalBayesError = bayesError(
@@ -55,6 +54,7 @@ def calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, pri
                               "--")
 
 
+# CALIBRATION SVM
 def calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False):
     C = 100
     gamma = 0.1
@@ -79,7 +79,7 @@ def calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False)
             lineLeft=-4,
             lineRight=4)
         createBayesErrorPlots(effPriorLogOdds, actDCFWithoutCalBayesError, minDCFWithoutCalBayesError, [-4, 4],
-                              [0, 1], "orange", "orange", f"QLR - calibration validation priorCal = {priorCal}",
+                              [0, 1], "orange", "orange", f"SVM - calibration validation priorCal = {priorCal}",
                               False, "actDCF", "min DCF", "-.",
                               ":")
         effPriorLogOdds, actDCFKFoldBayesError, minDCFKFoldBayesError = bayesError(
@@ -88,6 +88,50 @@ def calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False)
             lineLeft=-4,
             lineRight=4)
         createBayesErrorPlots(effPriorLogOdds, actDCFKFoldBayesError, minDCFKFoldBayesError, [-4, 4],
-                              [0, 1], "orange", "orange", f"QLR - calibration validation priorCal = {priorCal}",
+                              [0, 1], "orange", "orange", f"SVM - calibration validation priorCal = {priorCal}",
+                              True, "actDCF - cal", "min DCF - cal", "-",
+                              "--")
+
+
+# CALIBRATION GMM
+def calibrationGMM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False):
+    covType = "diagonal"
+    componentGMM0 = 8
+    componentGMM1 = 32
+    psi = 0.01
+    alpha = 0.1
+    sllrWithoutCal, minDCFWithoutCal, actDCFWithoutCal = trainGMMReturnMinAndActDCF(DTR, LTR, DVAL, LVAL,
+                                                                                    priorT, alpha,
+                                                                                    componentGMM0,
+                                                                                    componentGMM1, psi,
+                                                                                    covType)
+    minDCFKFold, actDCFKFold, llrK, labelK = trainGMMCalibrationReturnMinAndActDCF(K, priorCal, priorT,
+                                                                                   sllrWithoutCal, LVAL)
+
+    if printResult:
+        print("\tRESULT FOR CALIBRATION GMM")
+        print(
+            f"\tcovType: {covType}, component0 = {componentGMM0}, component1 = {componentGMM1}, psi: {psi}, alpha: {alpha}")
+        print(f"\t\tminDCF: {minDCFWithoutCal:.4f}")
+        print(f"\t\tactDCF: {actDCFWithoutCal:.4f}")
+        print(f"\t\tminDCF - cal: {minDCFKFold:.4f}")
+        print(f"\t\tactDCF - cal: {actDCFKFold:.4f}")
+
+        effPriorLogOdds, actDCFWithoutCalBayesError, minDCFWithoutCalBayesError = bayesError(
+            llr=sllrWithoutCal,
+            LTE=LVAL,
+            lineLeft=-4,
+            lineRight=4)
+        createBayesErrorPlots(effPriorLogOdds, actDCFWithoutCalBayesError, minDCFWithoutCalBayesError, [-4, 4],
+                              [0, 1], "green", "green", f"GMM - calibration validation priorCal = {priorCal}",
+                              False, "actDCF", "min DCF", "-.",
+                              ":")
+        effPriorLogOdds, actDCFKFoldBayesError, minDCFKFoldBayesError = bayesError(
+            llr=llrK,
+            LTE=labelK,
+            lineLeft=-4,
+            lineRight=4)
+        createBayesErrorPlots(effPriorLogOdds, actDCFKFoldBayesError, minDCFKFoldBayesError, [-4, 4],
+                              [0, 1], "green", "green", f"GMM - calibration validation priorCal = {priorCal}",
                               True, "actDCF - cal", "min DCF - cal", "-",
                               "--")
--- Project/Code/11. Lab 11/LBG.py
+++ Project/Code/11. Lab 11/LBG.py
@@ -1,7 +1,11 @@
 import numpy as np
+import scipy.optimize as sp
 
 from EMGMM import EMGmm, smoothCovariance
-from utils import compute_mu_C
+from utils import compute_mu_C, logpdf_GMM
+from DCF import minDCF, actDCF
+from PriorWeightedBinLogReg import priorWeightedLogClass
+from utils import vrow
 
 
 def LBGAlgorithm(X, alpha, numComponent, psi=None, covType="full"):
@@ -30,3 +34,35 @@ def split_GMM(gmm, alpha):
         gmmOut.append((w * 0.5, mu - d, C))
         gmmOut.append((w * 0.5, mu + d, C))
     return gmmOut
+
+
+# LAB 11
+def trainGMMReturnMinAndActDCF(DTR, LTR, DVAL, LVAL, priorT, alpha, componentGMM0, componentGMM1, psi, covType):
+    gmm0 = LBGAlgorithm(DTR[:, LTR == 0], alpha, componentGMM0, psi=psi, covType=covType)
+    gmm1 = LBGAlgorithm(DTR[:, LTR == 1], alpha, componentGMM1, psi=psi, covType=covType)
+    sllr = logpdf_GMM(DVAL, gmm1)[1] - logpdf_GMM(DVAL, gmm0)[1]
+    minDCFWithoutCal = minDCF(sllr, LVAL, priorT, 1.0, 1.0)
+    actDCFWithoutCal = actDCF(sllr, LVAL, priorT, 1.0, 1.0)
+    return sllr, minDCFWithoutCal, actDCFWithoutCal
+
+
+def trainGMMCalibrationReturnMinAndActDCF(K, priorCal, priorT, sllrWithoutCal, LVAL):
+    calibratedSVALK = []
+    labelK = []
+
+    for i in range(K):
+        SCAL, SVAL = np.hstack([sllrWithoutCal[jdx::K] for jdx in range(K) if jdx != i]), sllrWithoutCal[i::K]
+        labelCal, labelVal = np.hstack([LVAL[jdx::K] for jdx in range(K) if jdx != i]), LVAL[i::K]
+        logRegWeight = priorWeightedLogClass(vrow(SCAL), labelCal, 0, priorCal)
+        vf = \
+            sp.fmin_l_bfgs_b(func=logRegWeight.logreg_obj, x0=np.zeros(logRegWeight.DTR.shape[0] + 1))[0]
+        w, b = vf[:-1], vf[-1]
+        calibrated_SVAL = (w.T @ vrow(SVAL) + b - np.log(priorCal / (1 - priorCal))).ravel()
+        calibratedSVALK.append(calibrated_SVAL)
+        labelK.append(labelVal)
+
+    llrK = np.hstack(calibratedSVALK)
+    labelK = np.hstack(labelK)
+    minDCFKFold = minDCF(llrK, labelK, priorT, 1, 1)
+    actDCFKFold = actDCF(llrK, labelK, priorT, 1, 1)
+    return minDCFKFold, actDCFKFold, llrK, labelK
