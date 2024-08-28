e19aebecbbd256c7cf9b94121087d305c1272bb3
antonioior <antonio_ior@live.com>
2024-08-28 13:23:49 +0200

    Fix: Added fusion calibration

--- Project/Code/11. Lab 11/Calibration.py
+++ Project/Code/11. Lab 11/Calibration.py
@@ -15,9 +15,10 @@ def calibration(DTR, LTR, DVAL, LVAL, printResult=False):
     priorT = 0.1
     priorCals = [0.1, 0.5, 0.9]
     for priorCal in priorCals:
-        calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
-        calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
-        # calibrationGMM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
+        scoreQLR = calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
+        scoreSVM = calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
+        scoreGMM = calibrationGMM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult)
+        calibrationFusion(scoreQLR, scoreSVM, scoreGMM, LVAL, K, priorT, priorCal, printResult)
 
 
 # CALIBRATION LOGISTIC REGRESSION
@@ -33,6 +34,8 @@ def calibrationLogisticRegression(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, pri
         print(f"\tLambda: {l}")
         printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, score, LVAL, llrK, labelK,
                   f"QLR - calibration validation priorCal = {priorCal}", "b")
+
+    return score
 
 
 # CALIBRATION SVM
@@ -51,6 +54,7 @@ def calibrationSVM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False)
         print(f"\tC: {C}, \u03B3: {gamma}, KRadial: {KRadial}")
         printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, score, LVAL, llrK, labelK,
                   f"SVM - calibration validation priorCal = {priorCal}", "orange")
+    return score
 
 
 # CALIBRATION GMM
@@ -60,64 +64,65 @@ def calibrationGMM(DTR, LTR, DVAL, LVAL, K, priorT, priorCal, printResult=False)
     componentGMM1 = 32
     psi = 0.01
     alpha = 0.1
-    sllrWithoutCal, minDCFWithoutCal, actDCFWithoutCal = trainGMMReturnMinAndActDCF(DTR, LTR, DVAL, LVAL,
-                                                                                    priorT, alpha,
-                                                                                    componentGMM0,
-                                                                                    componentGMM1, psi,
-                                                                                    covType)
+    score, minDCFWithoutCal, actDCFWithoutCal = trainGMMReturnMinAndActDCF(DTR, LTR, DVAL, LVAL,
+                                                                           priorT, alpha,
+                                                                           componentGMM0,
+                                                                           componentGMM1, psi,
+                                                                           covType)
     minDCFKFold, actDCFKFold, llrK, labelK = trainGMMCalibrationReturnMinAndActDCF(K, priorCal, priorT,
-                                                                                   sllrWithoutCal, LVAL)
+                                                                                   score, LVAL)
 
     if printResult:
         print(f"\tRESULT FOR CALIBRATION GMM")
         print(
             f"\tcovType: {covType}, component0 = {componentGMM0}, component1 = {componentGMM1}, psi: {psi}, alpha: {alpha}")
-        printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, sllrWithoutCal, LVAL, llrK, labelK,
+        printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, score, LVAL, llrK, labelK,
                   f"GMM - calibration validation priorCal = {priorCal}", "green")
+    return score
 
 
 # CALIBRATION FUSION
-# def calibrationFusion(scoreQLR, scoreSVM, scoreGMM, LVAL, K, priorT, priorCal, printResult=False):
-#     calibratedSVALK = []
-#     labelK = []
-#
-#     for i in range(K):
-#         SCAL_QLR, SVAL_QLR = np.hstack([scoreQLR[jdx::K] for jdx in range(K) if jdx != i]), scoreQLR[i::K]
-#         SCAL_SVM, SVAL_SVM = np.hstack([scoreSVM[jdx::K] for jdx in range(K) if jdx != i]), scoreSVM[i::K]
-#         SCAL_GMM, SVAL_GMM = np.hstack([scoreGMM[jdx::K] for jdx in range(K) if jdx != i]), scoreGMM[i::K]
-#
-#         SCAL = np.hstack([SCAL_QLR, SCAL_SVM, SCAL_GMM])
-#         SVAL = np.hstack([SVAL_QLR, SVAL_SVM, SVAL_GMM])
-#
-#         labelCal, labelVal = np.hstack([LVAL[jdx::K] for jdx in range(K) if jdx != i]), LVAL[i::K]
-#         logRegWeight = priorWeightedLogClass(vrow(SCAL), labelCal, 0, priorCal)
-#         vf = \
-#             sp.fmin_l_bfgs_b(func=logRegWeight.logreg_obj, x0=np.zeros(logRegWeight.DTR.shape[0] + 1))[0]
-#         w, b = vf[:-1], vf[-1]
-#         calibrated_SVAL = (w.T @ vrow(SVAL) + b - np.log(priorCal / (1 - priorCal))).ravel()
-#         calibratedSVALK.append(calibrated_SVAL)
-#         labelK.append(labelVal)
-#
-#     llrK = np.hstack(calibratedSVALK)
-#     labelK = np.hstack(labelK)
-#     minDCFKFold = minDCF(llrK, labelK, priorT, 1, 1)
-#     actDCFKFold = actDCF(llrK, labelK, priorT, 1, 1)
-#
-#     if printResult:
-#         print(f"\tRESULT FOR CALIBRATION FUSION")
-#         print(f"\tminDCF - cal: {minDCFKFold:.4f}")
-#         print(f"\tactDCF - cal: {actDCFKFold:.4f}")
+def calibrationFusion(scoreQLR, scoreSVM, scoreGMM, LVAL, K, priorT, priorCal, printResult=False):
+    calibratedSVALK = []
+    labelK = []
+
+    for i in range(K):
+        SCAL_QLR, SVAL_QLR = np.hstack([scoreQLR[jdx::K] for jdx in range(K) if jdx != i]), scoreQLR[i::K]
+        SCAL_SVM, SVAL_SVM = np.hstack([scoreSVM[jdx::K] for jdx in range(K) if jdx != i]), scoreSVM[i::K]
+        SCAL_GMM, SVAL_GMM = np.hstack([scoreGMM[jdx::K] for jdx in range(K) if jdx != i]), scoreGMM[i::K]
+
+        SCAL = np.vstack([SCAL_QLR, SCAL_SVM, SCAL_GMM])
+        SVAL = np.vstack([SVAL_QLR, SVAL_SVM, SVAL_GMM])
+
+        labelCal, labelVal = np.hstack([LVAL[jdx::K] for jdx in range(K) if jdx != i]), LVAL[i::K]
+        logRegWeight = priorWeightedLogClass(SCAL, labelCal, 0, priorCal)
+        vf = \
+            sp.fmin_l_bfgs_b(func=logRegWeight.logreg_obj, x0=np.zeros(logRegWeight.DTR.shape[0] + 1))[0]
+        w, b = vf[:-1], vf[-1]
+        calibrated_SVAL = (w.T @ SVAL + b - np.log(priorCal / (1 - priorCal))).ravel()
+        calibratedSVALK.append(calibrated_SVAL)
+        labelK.append(labelVal)
+
+    llrK = np.hstack(calibratedSVALK)
+    labelK = np.hstack(labelK)
+    minDCFKFold = minDCF(llrK, labelK, priorT, 1, 1)
+    actDCFKFold = actDCF(llrK, labelK, priorT, 1, 1)
+
+    if printResult:
+        print(f"\tRESULT FOR CALIBRATION FUSION")
+        print(f"\tminDCF - cal: {minDCFKFold:.4f}")
+        print(f"\tactDCF - cal: {actDCFKFold:.4f}")
 
 
 # PRINT MAIN INFORMATION AND PLOT GRAPH NOT FOR FUSION
-def printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, sllrWithoutCal, LVAL, llrK, labelK,
+def printData(minDCFWithoutCal, actDCFWithoutCal, minDCFKFold, actDCFKFold, score, LVAL, llrK, labelK,
               titleGraph, colorGraph):
     print(f"\t\tminDCF: {minDCFWithoutCal:.4f}")
     print(f"\t\tactDCF: {actDCFWithoutCal:.4f}")
     print(f"\t\tminDCF - cal: {minDCFKFold:.4f}")
     print(f"\t\tactDCF - cal: {actDCFKFold:.4f}")
     effPriorLogOdds, actDCFWithoutCalBayesError, minDCFWithoutCalBayesError = bayesError(
-        llr=sllrWithoutCal,
+        llr=score,
         LTE=LVAL,
         lineLeft=-4,
         lineRight=4)
