Fix: GMM computation

Project/Code/11. Lab 11/EMGMM.py
@@ -1,59 +1,79 @@
 import numpy as np
 
-from utils import logpdf_GMM, vcol, vrow
+from utils import logpdf_GMM
 
 
-def EMGmm(X, gmm, threshold=1e-6, maxIter=1000000, psi=None, covType="full"):
-    log_likelihood = []
-    iteration = 0
-    newGMM = []
-    for _ in range(maxIter):
-        responsability, logLikelihoodReturned = E_Step(X, gmm)
-        gmm = M_Step(X, responsability, gmm, psi, covType)
-        log_likelihood.append(logLikelihoodReturned)
-        iteration += 1
-        if len(log_likelihood) > 1 and abs(log_likelihood[-1] - log_likelihood[-2]) < threshold:
-            newGMM = gmm
-            break
-    return log_likelihood, newGMM
+def EMGmm(X, gmm, psi=None, covType="full"):
+    thNew = None
+    thOld = None
+    N = X.shape[1]
+    D = X.shape[0]
 
+    while thOld == None or thNew - thOld > 1e-6:  # finchè non diverge
+        thOld = thNew
+        logSj, logSjMarg = logpdf_GMM(X, gmm)
+        thNew = np.sum(logSjMarg) / N
 
-def E_Step(X, gmm):
-    S, logdens = logpdf_GMM(X, gmm)
-    logResponsability = S - logdens
-    responsability = np.exp(logResponsability)
-    logLikelihood = logdens.mean()
-    return responsability, logLikelihood
+        P = np.exp(logSj - logSjMarg)  # Responsabilità che è uguale alla probabilita a posteriori
 
+        if covType == 'diagonal':
+            newGmm = []
+            for i in range(len(gmm)):
+                gamma = P[i, :]
+                Z = gamma.sum()
+                F = (gamma.reshape(1, -1) * X).sum(1)
+                S = np.dot(X, (gamma.reshape(1, -1) * X).T)
+                w = Z / N
+                mu = (F / Z).reshape(-1, 1)
+                sigma = S / Z - np.dot(mu, mu.T)
+                sigma *= np.eye(sigma.shape[0])
+                U, s, _ = np.linalg.svd(sigma)
+                s[s < psi] = psi
+                sigma = np.dot(U, s.reshape(-1, 1) * U.T)
+                newGmm.append((w, mu, sigma))
+            gmm = newGmm
 
-def M_Step(X, responsability, gmm, psi=None, covType="full"):
-    newGMM = []
-    for gIndex in range(len(gmm)):
-        gamma = responsability[gIndex]
-        Z = gamma.sum()
-        F = vcol((vrow(gamma) * X).sum(1))
-        S = (vrow(gamma) * X) @ X.T
-        mu = F / Z
-        C = S / Z - mu @ mu.T
-        w = Z / X.shape[1]
-        if covType == "diagonal":
-            C = C * np.eye(X.shape[0])
-        newGMM.append((w, mu, C))
+        elif covType == 'tied':
+            newGmm = []
+            sigmaTied = np.zeros((D, D))
+            for i in range(len(gmm)):
+                gamma = P[i, :]
+                Z = gamma.sum()
+                F = (gamma.reshape(1, -1) * X).sum(1)
+                S = np.dot(X, (gamma.reshape(1, -1) * X).T)
+                w = Z / N
+                mu = (F / Z).reshape(-1, 1)
+                sigma = S / Z - np.dot(mu, mu.T)
+                sigmaTied += Z * sigma
+                newGmm.append((w, mu))
+            gmm = newGmm
+            sigmaTied /= N
+            U, s, _ = np.linalg.svd(sigmaTied)
+            s[s < psi] = psi
+            sigmaTied = np.dot(U, s.reshape(-1, 1) * U.T)
 
-    if covType == "tied":
-        CTied = 0
-        for gIndex in range(len(gmm)):
-            wg, mug, Cg = newGMM[gIndex]
-            CTied += wg * Cg
-        newGMM = [(w, mu, CTied) for w, mu, C in newGMM]
+            newGmm = []
+            for i in range(len(gmm)):
+                (w, mu) = gmm[i]
+                newGmm.append((w, mu, sigmaTied))
 
-    if psi is not None:
-        newGMM = [(w, mu, smoothCovariance(C, psi)) for w, mu, C in newGMM]
+            gmm = newGmm
 
-    return newGMM
+        else:
+            newGmm = []
+            for i in range(len(gmm)):
+                gamma = P[i, :]
+                Z = gamma.sum()
+                F = (gamma.reshape(1, -1) * X).sum(1)
+                S = np.dot(X, (gamma.reshape(1, -1) * X).T)
 
+                w = Z / N
+                mu = (F / Z).reshape(-1, 1)
+                sigma = S / Z - np.dot(mu, mu.T)
+                U, s, _ = np.linalg.svd(sigma)
+                s[s < psi] = psi
+                sigma = np.dot(U, s.reshape(-1, 1) * U.T)
+                newGmm.append((w, mu, sigma))
+            gmm = newGmm
 
-def smoothCovariance(cov, psi):
-    U, s, _ = np.linalg.svd(cov)
-    s[s < psi] = psi
-    return U @ (vcol(s) * U.T)
+    return gmm, thNew

Project/Code/11. Lab 11/LBG.py
@@ -1,39 +1,33 @@
 import numpy as np
 import scipy.optimize as sp
 
-from EMGMM import EMGmm, smoothCovariance
-from utils import compute_mu_C, logpdf_GMM
+from EMGMM import EMGmm
+from utils import logpdf_GMM
 from DCF import minDCF, actDCF
 from PriorWeightedBinLogReg import priorWeightedLogClass
 from utils import vrow
 
 
 def LBGAlgorithm(X, alpha, numComponent, psi=None, covType="full"):
-    mu, C = compute_mu_C(X)
+    mean = X.mean(axis=1).reshape(-1, 1)
+    cov = 1 / X.shape[1] * np.dot(X - mean, (X - mean).T)
+    gmm = [(1.0, mean, cov)]
 
-    if covType == "diagonal":
-        C = C * np.eye(X.shape[0])
+    while len(gmm) <= numComponent:
+        gmm, _ = EMGmm(X, gmm, psi=psi, covType=covType)
+        if len(gmm) == numComponent:
+            break
 
-    if psi is not None:
-        gmm = [(1.0, mu, smoothCovariance(C, psi))]
-    else:
-        gmm = [(1.0, mu, C)]
+        newGmm = []
+        for i in range(len(gmm)):
+            (w, mu, sigma) = gmm[i]
+            U, s, Vh = np.linalg.svd(sigma)
+            d = U[:, 0:1] * s[0] ** 0.5 * alpha
 
-    while len(gmm) < numComponent:
-        gmm = split_GMM(gmm, alpha)
-        _, gmm = EMGmm(X, gmm, psi=psi, covType=covType)
+            newGmm.append((w / 2, mu - d, sigma))
+            newGmm.append((w / 2, mu + d, sigma))
+        gmm = newGmm
     return gmm
-
-
-def split_GMM(gmm, alpha):
-    gmmOut = []
-    for gIndex in range(len(gmm)):
-        w, mu, C = gmm[gIndex]
-        U, s, Vh = np.linalg.svd(C)
-        d = U[:, 0:1] * s[0] ** 0.5 * alpha
-        gmmOut.append((w * 0.5, mu - d, C))
-        gmmOut.append((w * 0.5, mu + d, C))
-    return gmmOut
 
 
 # LAB 11

Project/Code/11. Lab 11/utils.py
@@ -119,8 +119,6 @@ def compute_mu_C(D):
 
 
 # LAB 05
-
-
 def logpdf_GAU_ND_prof(x, mu, C):
     P = np.linalg.inv(C)
     return -0.5 * x.shape[0] * np.log(np.pi * 2) - 0.5 * np.linalg.slogdet(C)[1] - 0.5 * (
@@ -190,11 +188,16 @@ def z_normalizeOther(DT, D):
 
 # LAB 10
 def logpdf_GMM(X, gmm):
-    M = len(gmm)
-    N = X.shape[1]
-    S = np.zeros((M, N))
-    for g in range(M):
+    S = np.zeros((len(gmm), X.shape[1]))
+    for g in range(len(gmm)):
         w, mu, C = gmm[g]
-        S[g, :] = logpdf_GAU_ND(X, mu, C) + np.log(w)
+        S[g, :] = logpdf_GAU_ND_GMM(X, mu, C) + np.log(w)
     logdens = sp.special.logsumexp(S, axis=0)
     return S, logdens
+
+
+def logpdf_GAU_ND_GMM(X, mu, C):
+    invC = np.linalg.inv(C)
+    _, log_abs_detC = np.linalg.slogdet(C)
+    M = X.shape[0]
+    return - M / 2 * np.log(2 * np.pi) - 0.5 * log_abs_detC - 0.5 * ((X - mu) * np.dot(invC, X - mu)).sum(0)